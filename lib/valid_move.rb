c692d14feb75299ba2f9a4041f9a756e6ac28516
Julie  McManus <julieannesays@gmail.com>
2020-01-19 00:50:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ end
 
 def position_taken?(board, index)
   taken = nil
-  if (board[index]) == " " || board[index] == "" || board[index] == nil)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else
     taken = true
