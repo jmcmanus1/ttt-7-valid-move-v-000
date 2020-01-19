5042338795b5426dd846295a1a6de3ca210b596a
Julie  McManus <julieannesays@gmail.com>
2020-01-19 00:34:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  #if position.to_i.between?(0,8)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?
+end
